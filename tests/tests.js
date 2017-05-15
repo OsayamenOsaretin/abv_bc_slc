eaaedc42c121149f6c97ac142bcbdbee8c43b012
yamen <osaretinosayamen@gmail.com>
2017-05-15 18:19:07 +0100

    Add test suite for Negative Integer handling

--- tests/tests.js
+++ tests/tests.js
@@ -4,7 +4,7 @@
 
 const expect = require('jasmine-expect');
 
-describe("Invalid Input Handling", function(){
+describe("Invalid Input type Handling", function(){
 	// spec for testing invalid inputs (strings)
 	it("should return invalid input message for strings", function(){
 		expect(MyApp.getPrimeNumbers("string").toEqual("Invalid Input, please enter positive Integer"))
@@ -14,3 +14,20 @@ describe("Invalid Input Handling", function(){
 		expect(MyApp.getPrimeNumbers(4.2).toEqual("Invalid Input, please enter positive Integer"))
 	});
 });
+
+
+describe("Negative Integer Handling", function(){
+	//spec for testing negative integer inputs
+	it("should return negative integer input error message for -5", function(){
+		expect(MyApp.getPrimeNumbers(-5).toEqual("You used a negative Integer, please try positive Integers instead"))
+	});
+
+	it("should return negative integer input error message for -50", function(){
+		expect(MyApp.getPrimeNumbers(-50).toEqual("You used a negative Integer, please try positive Integers instead"))
+	});
+
+	it("should return negative integer input error message for -100", function(){
+		expect(MyApp.getPrimeNumbers(-100).toEqual("You used a negative Integer, please try positive Integers instead"))
+	});
+});
+
